8170e83b8388e9ef4f17c75da73ed9485cad6a24
olakehinde <olakehinde59@gmail.com>
2019-02-18 15:01:37 +0100

    redirect to appointments page after registering user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/appointments';
 
     /**
      * Create a new controller instance.
