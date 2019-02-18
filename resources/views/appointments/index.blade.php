87e932695bc27cc9e00bbd49ed86cf79db35d6ed
olakehinde <olakehinde59@gmail.com>
2019-02-18 16:52:14 +0100

    dumped data on the index page for testing

--- resources/views/appointments/index.blade.php
+++ resources/views/appointments/index.blade.php
@@ -1,6 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
+	<div class="container" id="calendar">
+		<div>
+			<a class="btn btn-info pull-right" href="{{route('appointments.create')}}">Create Appointment</a>
+		</div>
 
-	<h1>Hello, world!</h1>
+		@if(count($appointments) < 1)
+			<div>
+				<h2>You have no Appointments yet</h2>
+			</div>
+		@endif
+
+		@foreach($appointments as $appointment)
+			{{$appointment->appointment_date}}<br>
+			{{$appointment->appointment}}<br>
+		@endforeach
+	</div>
 @endsection
