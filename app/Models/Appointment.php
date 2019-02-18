970f54e318d3b369e715cd7e5a497c470029b503
olakehinde <olakehinde59@gmail.com>
2019-02-18 13:02:19 +0100

    added the fillable fields/data to the Appointment model file

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
-    //
+    protected $fillable = [
+    	'date',
+    	'user_id',
+    	'appointment'
+    ];
+
+    public function user() {
+    	return $this->belongsTo('App\Models\User');
+    }
 }
