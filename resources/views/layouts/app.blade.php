28bdd30c359e6ed3bf9e94707093459e3bd83987
olakehinde <olakehinde59@gmail.com>
2019-02-18 18:23:47 +0100

    finally fixed bug that was not making the calendar to show

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,7 +80,6 @@
 
     <!-- Scripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    @yield('script')    
-    <script src="{{ asset('js/app.js') }}"></script>
+    @yield('script')
 </body>
 </html>
